41ce375a7ae1879740ec721c2fb9b26acabcb959
foko <qkrrudgh0@naver.com>
2024-04-11 21:42:07 +0900

    feat:pageable을 OrderSpecifier로 바꾸는 기능 추가

--- src/main/kotlin/com/example/demo/common/QueryDslUtil.kt
+++ src/main/kotlin/com/example/demo/common/QueryDslUtil.kt
@@ -4,6 +4,7 @@ import com.querydsl.core.types.Order
 import com.querydsl.core.types.OrderSpecifier
 import com.querydsl.core.types.dsl.EntityPathBase
 import com.querydsl.core.types.dsl.PathBuilder
+import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 
 class QueryDslUtil {
@@ -17,6 +18,22 @@ class QueryDslUtil {
             val pathBuilder = PathBuilder(type.type, type.metadata)
 
             for (i in sort) {
+                val orderSpecifier =
+                    OrderSpecifier(if (i.isAscending) Order.ASC else Order.DESC, pathBuilder.getString(i.property))
+                resList.add(orderSpecifier)
+            }
+            return resList.toTypedArray()
+        }
+
+        fun <T : EntityPathBase<*>> pageableToOrderSpecifier(
+            pageable: Pageable,
+            type: T,
+        ): Array<OrderSpecifier<*>> {
+            val resList = mutableListOf<OrderSpecifier<*>>()
+
+            val pathBuilder = PathBuilder(type.type, type.metadata)
+
+            for (i in pageable.sort) {
                 val orderSpecifier =
                     OrderSpecifier(if (i.isAscending) Order.ASC else Order.DESC, pathBuilder.getString(i.property))
                 resList.add(orderSpecifier)
