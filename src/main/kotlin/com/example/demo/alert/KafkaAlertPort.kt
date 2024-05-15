141a46f3d7986347b95c457137e1ca1039a98176
foko <qkrrudgh0@naver.com>
2024-05-15 16:10:58 +0900

    refactor:카프카 토픽명을 상수클래스로 이동

--- src/main/kotlin/com/example/demo/alert/KafkaAlertPort.kt
+++ src/main/kotlin/com/example/demo/alert/KafkaAlertPort.kt
@@ -1,5 +1,6 @@
 package com.example.demo.alert
 
+import com.example.demo.constant.Constant
 import com.example.demo.domain.member.Member
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Component
 @Component
 class KafkaAlertPort(val template: KafkaTemplate<String, String>) : AlertPort {
     private val log = LoggerFactory.getLogger(this.javaClass)
-    private val topicName = "levelAlert"
 
     override fun send(member: Member) {
         val msg = """${member.id},${member.name},${member.level}"""
@@ -19,7 +19,7 @@ class KafkaAlertPort(val template: KafkaTemplate<String, String>) : AlertPort {
         key: Long,
         msg: String,
     ) {
-        val send = template.send(topicName, key.toString(), msg)
+        val send = template.send(Constant.TOPIC_NAME, key.toString(), msg)
         send.whenComplete { _, ex ->
             if (ex != null) {
                 log.warn("카프카 전송실패 key=$key 메시지=$msg 스택트레이스=${ex.stackTrace}")
--- src/main/kotlin/com/example/demo/constant/Constant.kt
+++ src/main/kotlin/com/example/demo/constant/Constant.kt
@@ -3,5 +3,6 @@ package com.example.demo.constant
 class Constant {
     companion object {
         const val SLACK_URL = "https://slack.com/api/api.test"
+        const val TOPIC_NAME = "levelAlert"
     }
 }
